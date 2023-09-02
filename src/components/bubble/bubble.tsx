1d686fb381ec17153c141a18ebfef0c63d0daa5a
Dario Candia <darcanf.94@gmail.com>
2023-09-01 23:17:02 -0400

    Added code to differentiate between normal text and code blocks in the Bubble component

--- src/components/bubble/bubble.tsx
+++ src/components/bubble/bubble.tsx
@@ -1,12 +1,32 @@
 import { type Message } from "ai";
 import styles from "./bubble.module.css";
+import CodeBlock from "../codeBlock/codeBlock";
 
 export default function Bubble({message}: {message: Message}){
     const bubbleClass = message.role === 'user' ? 'userBubble' : 'assistantBubble';
 
+    const segments = message.content.split(/(```[\s\S]*?```)/g);
+    
+
     return(
         <div key={message.id} className={`${styles.chatBubble} ${styles[bubbleClass]}`}>
-            <p>{message.content}</p>
+           {segments.map((segment, index) => {
+                if( message.role === 'assistant' && /^```[\s\S]*```$/.test(segment)){
+                    const language = segment.split('\n')[0].replace('```', '');
+                    const code = segment.replace(/```/g, '');
+                    return <CodeBlock key={index} value={code} language={language} isCode={true}/>
+                }else{
+                    const formattedSegments = segment.split(/(`[^`]+`)/g).map((part, i) => {
+                        if (part.startsWith('`')) {
+                          return <b key={i}>{part}</b>;
+                        } else {
+                          return part;
+                        }
+                      });
+                    
+                    return <p key={index}>{ formattedSegments }</p>;
+                }
+           })}
         </div>
     )
 }
--- src/components/codeBlock/codeBlock.tsx
+++ src/components/codeBlock/codeBlock.tsx
@@ -4,12 +4,17 @@ import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 interface CodeBlockProps{
     value: string;
     language: string;
+    isCode: boolean;
 }
 
-export default function CodeBlock({value, language}: CodeBlockProps){
-    return(
+export default function CodeBlock({value, language, isCode}: CodeBlockProps){
+     return isCode ? (
         <SyntaxHighlighter language={ language } style={ coldarkDark }>
             { value }
         </SyntaxHighlighter>
-    );
+    )
+    : (
+        <p>{value}</p>
+    )
+    ;
 }
