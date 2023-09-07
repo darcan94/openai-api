d6da25709b6c0f577d0756ca7125ea54784c1d56
Dario Candia <darcanf.94@gmail.com>
2023-09-06 21:18:46 -0400

    Add Header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header className="fixed top-0 flex items-center justify-between w-full h-16 px-4 bg-white/50  backdrop-blur-lg  blur-9.8 border border-opacity-45 z-10">
+            <h2>GPT 3.5</h2>
+        </header>
+    );
+}
