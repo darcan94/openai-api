2a5e3649c82fe3bc63a008b45b34b2df639d2653
Dario Candia <darcanf.94@gmail.com>
2023-11-04 20:24:07 -0300

    get chat list from mongodb

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,12 +2,10 @@ import { IconSidebarAlt } from "@/components/icons/Icons";
 import SidebarItem from "./sidebarItem";
 import { connectDB } from "@/utils/db";
 
-export const runtime = "edge";
  
 export default async function Sidebar({ children }: { children?: React.ReactNode }) {
-  const collection = await connectDB();
-  const chats = await collection.find().toArray();
-  console.log(chats);
+  const res = await fetch('http://localhost:3000/api/chats');
+  const {chats} = await res.json();
   
   return (
     <div className="hidden border-r bg-zinc-100/40 lg:block dark:bg-zinc-800/40 w-[320px]">
@@ -15,8 +13,10 @@ export default async function Sidebar({ children }: { children?: React.ReactNode
           <div className="flex h-[60px] items-center px-6 gap-5">
               <IconSidebarAlt/>
               <h2 className="">Chat history</h2>
-          </div>
-          <SidebarItem chat={ null }/>
+          </div>  
+          {chats.map((chat: any) => (
+                <SidebarItem key={chat.id} chat={chat}/>
+              ))}     
         </div>
       </div>
   );
--- src/components/sidebar/sidebarItem.tsx
+++ src/components/sidebar/sidebarItem.tsx
@@ -1,34 +1,15 @@
 import Link from "next/link";
 
-export default function SidebarItem({ chat }: { chat: any }) {
+export default function SidebarItem({ chat }: { chat: any }) {  
   return (
     <div className="flex-1 overflow-y-auto">
       <nav className="grid gap-4 items-start px-4 text-sm font-medium">
-        <Link href="/">
+        <Link className="no-underline" href={`/chat/${chat._id}`}>
           <div className="flex items-center gap-3 rounded-lg bg-zinc-100 px-3 py-2 text-zinc-900 transition-all hover:text-zinc-900 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:text-zinc-50">
-            Contact Name
+            <span className="font-extralight">{chat.title}</span>
           </div>
         </Link>  
       </nav>
     </div>
   );
 }
-
-/**
- *  <div>
-      <Link href="/">
-        <div
-          className="relative max-h-5 flex-1 select-none overflow-hidden text-ellipsis break-all"
-          title={chat.title}
-        >
-          <span className="whitespace-nowrap">{chat.title}</span>
-        </div>
-      </Link>
-    </div>
-
-
-
-    <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-zinc-500 transition-all hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50">
-          Contact Name
-        </div>
- */
