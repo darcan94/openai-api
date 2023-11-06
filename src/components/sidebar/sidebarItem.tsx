b746aad06f6fd2435735db14c4f78cc60c3ca4ed
darcan94 <darcanf.94@gmail.com>
2023-11-06 15:36:16 -0300

    Adjust sidebarItem

--- src/components/sidebar/sidebarItem.tsx
+++ src/components/sidebar/sidebarItem.tsx
@@ -4,9 +4,9 @@ export default function SidebarItem({ chat }: { chat: any }) {
   return (
     <div key={chat._id} className="flex-1 w-full">
       <nav className="grid gap-4 items-start px-4 text-sm font-medium">
-        <Link className="no-underline" href={`/chat/${chat._id}`}>
+        <Link className="no-underline overflow-hidden" href={`/chat/${chat._id}`}>
           <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-zinc-500 transition-all hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50 hover:bg-zinc-100">
-            <span className="font-extralight">{chat.title}</span>
+            <span className="font-extralight whitespace-nowrap">{chat.title}</span>
           </div>
         </Link>  
       </nav>
