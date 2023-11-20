e4ce295af313e2fd10003768d9fd8e1d830527bc
darcan94 <darcanf.94@gmail.com>
2023-11-20 16:07:33 -0300

    Add: delete chat endpoint

---
+++ src/app/api/chats/[id]/route.ts
@@ -0,0 +1,17 @@
+import { DeleteChatsService } from "@/app/modules/chats/application/DeleteChatService";
+import { ChatRepositoryImpl } from "@/app/modules/chats/infra/ChatRepositoryImpl";
+import { ObjectId } from "mongodb";
+import { NextResponse } from "next/server";
+
+const chatRepositoryImpl = new ChatRepositoryImpl()
+const deleteChat = new DeleteChatsService(chatRepositoryImpl);
+
+export const DELETE = async (request: Request, {params}: {params: {id: ObjectId}}) => {
+    const chatId: ObjectId = params.id;
+    const chatDeleted: number = await deleteChat.execute(chatId);
+    
+    return NextResponse.json({
+        chatDeleted: chatDeleted,
+    });
+    
+};
--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 import { GetAllChatsService } from "@/app/modules/chats/application/GetAllChatService";
 import { ChatRepositoryImpl } from "@/app/modules/chats/infra/ChatRepositoryImpl";
-import { DeleteChatsService } from "@/app/modules/chats/application/DeleteChatService";
 
 const chatRepositoryImpl = new ChatRepositoryImpl()
 const getAllChats = new GetAllChatsService(chatRepositoryImpl);
-const deleteChat = new DeleteChatsService(chatRepositoryImpl);
 
 export const GET = async () => {
   const chats = getAllChats.execute();
@@ -13,10 +11,4 @@ export const GET = async () => {
   return NextResponse.json({
     chats: await chats,
   });
-};
-
-export const DELETE = async (request: Request) => {
- // const chatDeleted: number = await deleteChat.execute(request.params.id);
- console.log(request);
- 
 };
