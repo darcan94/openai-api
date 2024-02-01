379fda873e67e278ff2a22256c6855b03470022d
darcan94 <darcanf.94@gmail.com>
2024-02-01 15:34:02 -0300

    Adjust toggleSidebar

--- src/app/components/sidebar/sidebarToggle.tsx
+++ src/app/components/sidebar/sidebarToggle.tsx
@@ -2,17 +2,23 @@
 import { Button } from "@/app/components/ui/Button";
 import { IconSidebarAlt } from "@/app/components/ui/Icons";
 import { useSidebar } from "../sidebarProvider";
+import { createPortal } from "react-dom";
 
 export function SidebarToggle(){
-    const { toggleSidebar } = useSidebar();
+    const { isSidebarOpen, toggleSidebar } = useSidebar();
+
+    const toggleButton = <Button 
+                            variant="ghost" 
+                            size="icon"
+                            className={`fixed top-0 z-10 transition-all duration-300 ${isSidebarOpen ? 'left-[350px]' : 'left-0'}`}
+                            onClick={() => toggleSidebar()}>
+                                <IconSidebarAlt />
+                                <span className="sr-only">Toggle Sidebar</span>
+                        </Button>
 
     return(
-        <Button 
-            variant="ghost" 
-            size="icon"
-            onClick={() => toggleSidebar()}>
-                <IconSidebarAlt />
-                <span className="sr-only">Toggle Sidebar</span>
-        </Button>
+        <>
+            {createPortal(toggleButton, document.body)}
+        </>        
     );
 }
