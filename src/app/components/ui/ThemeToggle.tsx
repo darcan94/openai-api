3f32effabe4b8c879b7df9ec1addbc9edde2d394
Dario Candia <darcanf.94@gmail.com>
2024-02-14 20:58:13 -0300

    Adjust toggleTheme

--- src/app/components/ui/ThemeToggle.tsx
+++ src/app/components/ui/ThemeToggle.tsx
@@ -3,15 +3,9 @@ import { useTheme } from "next-themes";
 import { Button } from "@/app/components/ui/Button";
 import { IconMoon, IconSun, IconSystem } from "@/app/components/ui/Icons";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
 
 export default function ThemeToggle(){
     const {theme, setTheme} = useTheme();
-    const [mounted, setMounted] = useState(false);
-    
-    useEffect(() => {
-        setMounted(true);
-    }, []);
 
     return (
         <div className="flex justify-center">
@@ -19,9 +13,7 @@ export default function ThemeToggle(){
                 variant="ghost" 
                 size="icon" 
                 onClick={() => setTheme('dark')}
-                className={clsx("rounded-full", {
-                     "bg-background": mounted && theme === 'dark' 
-                })}>
+                className={clsx("rounded-full", {"bg-background": theme === 'dark'})}>
                     <IconMoon/>
                     <span className="sr-only">Toggle light theme</span>
             </Button>
@@ -30,9 +22,7 @@ export default function ThemeToggle(){
                 variant="ghost" 
                 size="icon" 
                 onClick={() => setTheme('system')}
-                className={clsx("rounded-full", {
-                    "bg-background": mounted && theme === 'system' 
-               })}>
+                className={clsx("rounded-full", { "bg-background": theme === 'system'})}>
                     <IconSystem/>
                     <span className="sr-only">Toggle system theme</span>
             </Button>
@@ -41,9 +31,7 @@ export default function ThemeToggle(){
                 variant="ghost" 
                 size="icon" 
                 onClick={() => setTheme('light')}
-                className={clsx("rounded-full", {
-                    "bg-background": mounted && theme === 'light' 
-                })}>
+                className={clsx("rounded-full", {"bg-background": theme === 'light'})}>
                     <IconSun/>
                     <span className="sr-only">Toggle dark theme</span>
             </Button>
