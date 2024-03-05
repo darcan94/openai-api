f59ae8575787c9ca30477c361e3fe7bf12e1d7dd
darcan94 <darcanf.94@gmail.com>
2024-03-05 15:58:30 -0300

    Add some styles to modelSelector

--- src/app/components/modelSelector/modelSelector.tsx
+++ src/app/components/modelSelector/modelSelector.tsx
@@ -1,7 +1,7 @@
 export default function ModelSelector(){
     return(
-        <div>
-            <select name="select">
+        <div className="rounded-md p-2 text-xl w-fit">
+            <select name="select" className="bg-transparent focus:outline-none">
                 <option value="chat">gpt-3.5</option>
                 <option value="chatGemini">gemini-pro</option>
             </select>
